6b3b887f284f9e9b0c0d92db9e1db111d2ec5f35
Or Bruchim <orbruhim@gmail.com>
2022-12-24 00:16:26 +0200

    install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { LoginModule } from "./login/login.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule],
+  imports: [BrowserModule, AppRoutingModule, LoginModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
